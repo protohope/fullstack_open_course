e3656a21d977dc1a86a40cdfd2ea498138e385f3
protohope <protohope.dev@gmail.com>
2025-01-07 17:23:15 +0300

    1.8: unicafe step 3 added

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,22 +16,39 @@ const FeedbackStatistics = ({fieldText, value}) => {
   )
 }
 
+const Statistics = (
+  {goodOptionText, neutralOptionText, badOptionText,
+    good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = all == 0 ? 0 : ((good - bad) / all)
+  const positive = all == 0 ? 0 : 100 * good / all
+
+  const allText = 'all'
+  const averageText = 'average'
+  const positiveText = 'positive'
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <FeedbackStatistics fieldText={goodOptionText} value={good} />
+      <FeedbackStatistics fieldText={neutralOptionText} value={neutral} />
+      <FeedbackStatistics fieldText={badOptionText} value={bad} />
+      <FeedbackStatistics fieldText={allText} value={all} />
+      <FeedbackStatistics fieldText={averageText} value={average} />
+      <FeedbackStatistics fieldText={positiveText} value={positive + '%'} />
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const all = good + neutral + bad
-  const average = all == 0 ? 0 : ((good - bad) / all)
-  const positive = all == 0 ? 0 : 100 * good / all
-
   const goodOptionText = 'good'
   const neutralOptionText = 'neutral'
   const badOptionText = 'bad'
-  const allText = 'all'
-  const averageText = 'average'
-  const positiveText = 'positive'
 
   const handleGood = () => {
     setGood(good + 1)
@@ -51,13 +68,14 @@ const App = () => {
       <FeedbackButton onClick={handleGood} text={goodOptionText} />
       <FeedbackButton onClick={handleNeutral} text={neutralOptionText} />
       <FeedbackButton onClick={handleBad} text={badOptionText} />
-      <h1>statistics</h1>
-      <FeedbackStatistics fieldText={goodOptionText} value={good} />
-      <FeedbackStatistics fieldText={neutralOptionText} value={neutral} />
-      <FeedbackStatistics fieldText={badOptionText} value={bad} />
-      <FeedbackStatistics fieldText={allText} value={all} />
-      <FeedbackStatistics fieldText={averageText} value={average} />
-      <FeedbackStatistics fieldText={positiveText} value={positive + '%'} />
+      <Statistics
+        goodOptionText={goodOptionText}
+        neutralOptionText={neutralOptionText}
+        badOptionText={badOptionText}
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
     </div>
   )
 }
